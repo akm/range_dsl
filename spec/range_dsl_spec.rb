3ae5ca624abf4d64f7487d2a5fb028d6154e529f
akimatter <akm2000@gmail.com>
2010-09-18 14:42:20 +0900

    インデントを一つ減らしました。

--- spec/range_dsl_spec.rb
+++ spec/range_dsl_spec.rb
@@ -7,201 +7,199 @@ describe "RangeDsl" do
     @context.extend(RangeDsl)
   end
 
-  describe "introduction" do
-    describe "easy open range" do
-      describe "gte" do
-        [:gte, :greater_than_equal].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(99).should == false
-            @r1.include?(100).should == true
-            @r1.include?(100.0).should == true
-            @r1.include?(1234567890).should == true
-          end
-        end
-      end
-
-      describe "gt" do
-        [:gt, :greater_than].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(99).should == false
-            @r1.include?(100).should == false
-            @r1.include?(100.0).should == false
-            @r1.include?(1234567890).should == true
-          end
-        end
-      end
-
-      describe "lte" do
-        [:lte, :less_than_equal].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(-1234567890).should == true
-            @r1.include?(99).should == true
-            @r1.include?(99.99).should == true
-            @r1.include?(100).should == true
-            @r1.include?(100.0).should == true
-            @r1.include?(100.000001).should == false
-            @r1.include?(101).should == false
-          end
-        end
-      end
-
-      describe "lt" do
-        [:lt, :less_than].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(-1234567890).should == true
-            @r1.include?(99).should == true
-            @r1.include?(99.99).should == true
-            @r1.include?(100).should == false
-            @r1.include?(100.0).should == false
-            @r1.include?(100.000001).should == false
-            @r1.include?(101).should == false
-          end
+  describe "easy open range" do
+    describe "gte" do
+      [:gte, :greater_than_equal].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(99).should == false
+          @r1.include?(100).should == true
+          @r1.include?(100.0).should == true
+          @r1.include?(1234567890).should == true
         end
       end
     end
 
-    describe "just equal, or just not equal" do
-      describe "eq" do
-        [:eq, :equal].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(99).should == false
-            @r1.include?(99.9999).should == false
-            @r1.include?(100.0).should == true
-            @r1.include?(100).should == true
-            @r1.include?(100.0001).should == false
-            @r1.include?(101).should == false
-          end
-        end
-      end
-
-      describe "lt" do
-        [:neq, :not_equal].each do |operator|
-          it operator do
-            @r1 = @context.send(operator, 100)
-            @r1.include?(99).should == true
-            @r1.include?(99.9999).should == true
-            @r1.include?(100.0).should == false
-            @r1.include?(100).should == false
-            @r1.include?(100.0001).should == true
-            @r1.include?(101).should == true
-          end
+    describe "gt" do
+      [:gt, :greater_than].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(99).should == false
+          @r1.include?(100).should == false
+          @r1.include?(100.0).should == false
+          @r1.include?(1234567890).should == true
         end
       end
     end
 
-    describe "not with other expression" do
-      it "not equal(100)" do
-        r1 = @context.instance_eval do
-          not_be(equal(100))
+    describe "lte" do
+      [:lte, :less_than_equal].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(-1234567890).should == true
+          @r1.include?(99).should == true
+          @r1.include?(99.99).should == true
+          @r1.include?(100).should == true
+          @r1.include?(100.0).should == true
+          @r1.include?(100.000001).should == false
+          @r1.include?(101).should == false
         end
-        r1.include?(99).should == true
-        r1.include?(99.9).should == true
-        r1.include?(100).should == false
-        r1.include?(100.0).should == false
-        r1.include?(100.1).should == true
-        r1.include?(101).should == true
       end
-
     end
 
-
-    describe "complex pattern" do
-      describe "gt(3) & lt(6)" do
-        ["gt(3).and(lt(6))", "gt(3) & lt(6)"].each do |dsl|
-          it dsl do
-            @r2 = @context.instance_eval(dsl)
-            @r2.include?(2).should == false
-            @r2.include?(2.999).should == false
-            @r2.include?(3).should == false
-            @r2.include?(3.0).should == false
-            @r2.include?(3.1).should == true
-            @r2.include?(4).should == true
-            @r2.include?(5).should == true
-            @r2.include?(5.9).should == true
-            @r2.include?(6.0).should == false
-            @r2.include?(6).should == false
-            @r2.include?(6.1).should == false
-            @r2.include?(7).should == false
-          end
+    describe "lt" do
+      [:lt, :less_than].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(-1234567890).should == true
+          @r1.include?(99).should == true
+          @r1.include?(99.99).should == true
+          @r1.include?(100).should == false
+          @r1.include?(100.0).should == false
+          @r1.include?(100.000001).should == false
+          @r1.include?(101).should == false
         end
       end
+    end
+  end
 
-      describe "gte(3) & lte(6)" do
-        ["gte(3).and(lte(6))", "gte(3) & lte(6)"].each do |dsl|
-          it dsl do
-            @r2 = @context.instance_eval(dsl)
-            @r2.include?(2).should == false
-            @r2.include?(2.999).should == false
-            @r2.include?(3).should == true
-            @r2.include?(3.0).should == true
-            @r2.include?(3.1).should == true
-            @r2.include?(4).should == true
-            @r2.include?(5).should == true
-            @r2.include?(5.9).should == true
-            @r2.include?(6.0).should == true
-            @r2.include?(6).should == true
-            @r2.include?(6.1).should == false
-            @r2.include?(7).should == false
-          end
+  describe "just equal, or just not equal" do
+    describe "eq" do
+      [:eq, :equal].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(99).should == false
+          @r1.include?(99.9999).should == false
+          @r1.include?(100.0).should == true
+          @r1.include?(100).should == true
+          @r1.include?(100.0001).should == false
+          @r1.include?(101).should == false
         end
       end
+    end
 
-      describe "lt(3) | gt(6)" do
-        ["lt(3).or(gt(6))", "lt(3) | gt(6)"].each do |dsl|
-          it dsl do
-            @r2 = @context.instance_eval(dsl)
-            @r2.include?(2).should == true
-            @r2.include?(2.999).should == true
-            @r2.include?(3).should == false
-            @r2.include?(3.0).should == false
-            @r2.include?(3.1).should == false
-            @r2.include?(4).should == false
-            @r2.include?(5).should == false
-            @r2.include?(5.9).should == false
-            @r2.include?(6.0).should == false
-            @r2.include?(6).should == false
-            @r2.include?(6.1).should == true
-            @r2.include?(7).should == true
-          end
+    describe "lt" do
+      [:neq, :not_equal].each do |operator|
+        it operator do
+          @r1 = @context.send(operator, 100)
+          @r1.include?(99).should == true
+          @r1.include?(99.9999).should == true
+          @r1.include?(100.0).should == false
+          @r1.include?(100).should == false
+          @r1.include?(100.0001).should == true
+          @r1.include?(101).should == true
         end
       end
+    end
+  end
 
-      describe "lte(3) | gte(6)" do
-        [
-          "lte(3).or(gte(6))",
-          "lte(3) | gte(6)",
-          # not_be を使う
-          "not_be(gt(3) & lt(6))",
-          "not_be(gt(3).and(lt(6)))",
-          # 更にド・モルガンの法則で展開
-          "not_be(gt(3)) | not_be(lt(6))",
-          "not_be(gt(3)).or(not_be(lt(6)))",
-        ].each do |dsl|
-          it dsl do
-            @r2 = @context.instance_eval(dsl)
-            @r2.include?(2).should == true
-            @r2.include?(2.999).should == true
-            @r2.include?(3).should == true
-            @r2.include?(3.0).should == true
-            @r2.include?(3.1).should == false
-            @r2.include?(4).should == false
-            @r2.include?(5).should == false
-            @r2.include?(5.9).should == false
-            @r2.include?(6.0).should == true
-            @r2.include?(6).should == true
-            @r2.include?(6.1).should == true
-            @r2.include?(7).should == true
-          end
-        end
+  describe "not with other expression" do
+    it "not equal(100)" do
+      r1 = @context.instance_eval do
+        not_be(equal(100))
       end
-
+      r1.include?(99).should == true
+      r1.include?(99.9).should == true
+      r1.include?(100).should == false
+      r1.include?(100.0).should == false
+      r1.include?(100.1).should == true
+      r1.include?(101).should == true
     end
 
   end
+
+
+  describe "complex pattern" do
+    describe "gt(3) & lt(6)" do
+      ["gt(3).and(lt(6))", "gt(3) & lt(6)"].each do |dsl|
+        it dsl do
+          @r2 = @context.instance_eval(dsl)
+          @r2.include?(2).should == false
+          @r2.include?(2.999).should == false
+          @r2.include?(3).should == false
+          @r2.include?(3.0).should == false
+          @r2.include?(3.1).should == true
+          @r2.include?(4).should == true
+          @r2.include?(5).should == true
+          @r2.include?(5.9).should == true
+          @r2.include?(6.0).should == false
+          @r2.include?(6).should == false
+          @r2.include?(6.1).should == false
+          @r2.include?(7).should == false
+        end
+      end
+    end
+
+    describe "gte(3) & lte(6)" do
+      ["gte(3).and(lte(6))", "gte(3) & lte(6)"].each do |dsl|
+        it dsl do
+          @r2 = @context.instance_eval(dsl)
+          @r2.include?(2).should == false
+          @r2.include?(2.999).should == false
+          @r2.include?(3).should == true
+          @r2.include?(3.0).should == true
+          @r2.include?(3.1).should == true
+          @r2.include?(4).should == true
+          @r2.include?(5).should == true
+          @r2.include?(5.9).should == true
+          @r2.include?(6.0).should == true
+          @r2.include?(6).should == true
+          @r2.include?(6.1).should == false
+          @r2.include?(7).should == false
+        end
+      end
+    end
+
+    describe "lt(3) | gt(6)" do
+      ["lt(3).or(gt(6))", "lt(3) | gt(6)"].each do |dsl|
+        it dsl do
+          @r2 = @context.instance_eval(dsl)
+          @r2.include?(2).should == true
+          @r2.include?(2.999).should == true
+          @r2.include?(3).should == false
+          @r2.include?(3.0).should == false
+          @r2.include?(3.1).should == false
+          @r2.include?(4).should == false
+          @r2.include?(5).should == false
+          @r2.include?(5.9).should == false
+          @r2.include?(6.0).should == false
+          @r2.include?(6).should == false
+          @r2.include?(6.1).should == true
+          @r2.include?(7).should == true
+        end
+      end
+    end
+
+    describe "lte(3) | gte(6)" do
+      [
+        "lte(3).or(gte(6))",
+        "lte(3) | gte(6)",
+        # not_be を使う
+        "not_be(gt(3) & lt(6))",
+        "not_be(gt(3).and(lt(6)))",
+        # 更にド・モルガンの法則で展開
+        "not_be(gt(3)) | not_be(lt(6))",
+        "not_be(gt(3)).or(not_be(lt(6)))",
+      ].each do |dsl|
+        it dsl do
+          @r2 = @context.instance_eval(dsl)
+          @r2.include?(2).should == true
+          @r2.include?(2.999).should == true
+          @r2.include?(3).should == true
+          @r2.include?(3.0).should == true
+          @r2.include?(3.1).should == false
+          @r2.include?(4).should == false
+          @r2.include?(5).should == false
+          @r2.include?(5.9).should == false
+          @r2.include?(6.0).should == true
+          @r2.include?(6).should == true
+          @r2.include?(6.1).should == true
+          @r2.include?(7).should == true
+        end
+      end
+    end
+
+  end
+
 
 end
